3dc0ff94d1e58142965fe7936767750872743f7e
LOYISO MJOLI <loyisovctr@gmail.com>
2025-10-19 23:54:22 +0200

    task 4

--- task_4.sql
+++ task_4.sql
@@ -1,4 +1,8 @@
+-- task_4.sql
+-- This script prints the full description of the 'Books' table
+-- from the 'alx_book_store' database without using DESCRIBE or EXPLAIN
+
 SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
 FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = DATABASE()
-  AND TABLE_NAME = 'books';
+WHERE TABLE_SCHEMA = 'alx_book_store'
+  AND TABLE_NAME = 'Books';
