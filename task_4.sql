d2b0fca78b7637d8051b9c94503434ddca414fd8
LOYISO MJOLI <loyisovctr@gmail.com>
2025-10-19 23:59:26 +0200

    task 4

--- task_4.sql
+++ task_4.sql
@@ -1,8 +1,15 @@
 -- task_4.sql
 -- This script prints the full description of the 'Books' table
--- from the 'alx_book_store' database without using DESCRIBE or EXPLAIN
+-- from the 'alx_book_store' database
+-- You are not allowed to use DESCRIBE or EXPLAIN statements
 
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
+SELECT
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_KEY,
+    COLUMN_DEFAULT,
+    EXTRA
 FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_SCHEMA = 'alx_book_store'
   AND TABLE_NAME = 'Books';
